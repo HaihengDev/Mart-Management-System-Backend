4c3a98a29cbeed97278ed32f36f81f82e17a3fb5
HaihengDev <haihenglim9@gmail.com>
2026-04-23 15:17:25 +0700

    feat: add MySQL2 dependency and configure database connection

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
-import express from "express";
-import { createServer } from "http";
-import morgan from "morgan";
-import dotenv from "dotenv";
+import express, { Request, Response } from 'express';
+import { createServer } from 'http';
+import dotenv from 'dotenv';
+import pool from './config/db';
+
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+app.get('/users', async (req: Request, res: Response) => {
+  try {
+    const [rows] = await pool.query<any[]>('SELECT * FROM tbStudent');
+    res.status(200).json(rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Database Error' });
+  }
+});
+
+const PORT = process.env.PORT || 8888;
 const server = createServer(app);
 
-server.listen(process.env.PORT, () => {
-  console.log("server is running on port 8888...");
+server.listen(PORT, () => {
+  console.log('server is running on port 8888...');
 });
