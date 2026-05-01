444cbbd6e30a77f16ddee1d76e6b5076f67a2807
HaihengDev <haihenglim9@gmail.com>
2026-05-01 17:44:39 +0700

    feat: add employee interface and model with schema definition for employee management

---
+++ src/interfaces/employee.interface.ts
@@ -0,0 +1,7 @@
+export interface IEmployee {
+  employee_id: Number;
+  employee_name: String;
+  gender: 'Male' | 'Female';
+  position: 'employee' | 'admin';
+  salary: Number;
+}
---
+++ src/models/employee.model.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+import { IEmployee } from '../interfaces/employee.interface';
+
+const employeeSchema = new Schema<IEmployee>({
+  employee_id: {
+    type: Number,
+    unique: true,
+    required: true,
+  },
+  employee_name: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    enum: ['Male', 'Female'],
+    required: true,
+  },
+  position: {
+    type: String,
+    enum: ['employee', 'admin'],
+    required: true,
+  },
+  salary: {
+    type: Number,
+    required: true,
+  },
+});
